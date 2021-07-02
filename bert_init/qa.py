ca660702a5a24963a39820e79ee35da36e0384c9
dav-l <david94306@gmail.com>
2021-07-02 08:38:31 -0700

    Update qa.py

--- bert_init/qa.py
+++ bert_init/qa.py
@@ -9,88 +9,131 @@ transformers_logger.setLevel(logging.WARNING)
 
 
 def main():
-    train_data = [
+   train_data = [
+    {
+        "context": "2021 Astronics Corporation Annual Meeting of Shareholders",
+        "qas": [
+            {
+                "id": "00001",
+                "is_impossible": False,
+                "question": "What is the event type?",
+                "answers": [
+                    {
+                        "text": "Annual Meeting of Shareholders",
+                        "answer_start": 28,
+                    }
+                ],
+            }
+        ],
+    },
+    {
+        "context": "The first series, published between 2006 and 2008, consists of The Final Empire,"
+                "The Well of Ascension, and The Hero of Ages.",
+        "qas": [
+            {
+                "id": "00002",
+                "is_impossible": False,
+                "question": "When was the series published?",
+                "answers": [
+                    {
+                        "text": "between 2006 and 2008",
+                        "answer_start": 28,
+                    }
+                ],
+            },
+        ],
+    },
+]
+
+eval_data = [
+    {
+        "context": "The series primarily takes place in a region called the Final Empire "
+                "on a world called Scadrial, where the sun and sky are red, vegetation is brown, "
+                "and the ground is constantly being covered under black volcanic ashfalls.",
+        "qas": [
+            {
+                "id": "00001",
+                "is_impossible": False,
+                "question": "Where does the series take place?",
+                "answers": [
+                    {
+                        "text": "region called the Final Empire",
+                        "answer_start": 38,
+                    },
+                    {
+                        "text": "world called Scadrial",
+                        "answer_start": 74,
+                    },
+                ],
+            }
+        ],
+    },
+    {
+        "context": "\"Mistings\" have only one of the many Allomantic powers, while \"Mistborns\" have all the powers.",
+        "qas": [
+            {
+                "id": "00002",
+                "is_impossible": False,
+                "question": "How many powers does a Misting possess?",
+                "answers": [
+                    {
+                        "text": "one",
+                        "answer_start": 21,
+                    }
+                ],
+            },
+            {
+                "id": "00003",
+                "is_impossible": True,
+                "question": "What are Allomantic powers?",
+                "answers": [],
+            },
+        ],
+    },
+]
+
+# Configure the model
+model_args = QuestionAnsweringArgs()
+model_args.train_batch_size = 16
+model_args.evaluate_during_training = True
+model_args.overwrite_output_dir = True
+#model_args.fp16 = False
+
+model = QuestionAnsweringModel(
+    "distilbert", "distilbert-base-uncased", args=model_args
+)
+
+# Train the model
+model.train_model(train_data, eval_data=eval_data)
+
+# Evaluate the model
+result, texts = model.eval_model(eval_data)
+
+    # Make predictions with the model
+to_predict = [
         {
-            "context": "Mistborn is a series of epic fantasy novels written by American author Brandon Sanderson.",
+            "context": "2021 Astronics Corporation Annual Meeting of Shareholders.",
             "qas": [
                 {
-                    "id": "00001",
-                    "is_impossible": False,
-                    "question": "Who is the author of the Mistborn series?",
-                    "answers": [
-                        {
-                            "text": "Brandon Sanderson",
-                            "answer_start": 71,
-                        }
-                    ],
+                    "question": "What is the event type?",
+                    "id": "0",
                 }
             ],
         },
-        {
-            "context": "The first series, published between 2006 and 2008, consists of The Final Empire,"
-                    "The Well of Ascension, and The Hero of Ages.",
+                {
+            "context": "Fourth Quarter 2020 Earnings.",
             "qas": [
                 {
-                    "id": "00002",
-                    "is_impossible": False,
-                    "question": "When was the series published?",
-                    "answers": [
-                        {
-                            "text": "between 2006 and 2008",
-                            "answer_start": 28,
-                        }
-                    ],
-                },
+                    "question": "What is the event type?",
+                    "id": "1",
+                }
             ],
         },
     ]
 
-    eval_data = [
-        {
-            "context": "The series primarily takes place in a region called the Final Empire "
-                    "on a world called Scadrial, where the sun and sky are red, vegetation is brown, "
-                    "and the ground is constantly being covered under black volcanic ashfalls.",
-            "qas": [
-                {
-                    "id": "00001",
-                    "is_impossible": False,
-                    "question": "Where does the series take place?",
-                    "answers": [
-                        {
-                            "text": "region called the Final Empire",
-                            "answer_start": 38,
-                        },
-                        {
-                            "text": "world called Scadrial",
-                            "answer_start": 74,
-                        },
-                    ],
-                }
-            ],
-        },
-        {
-            "context": "\"Mistings\" have only one of the many Allomantic powers, while \"Mistborns\" have all the powers.",
-            "qas": [
-                {
-                    "id": "00002",
-                    "is_impossible": False,
-                    "question": "How many powers does a Misting possess?",
-                    "answers": [
-                        {
-                            "text": "one",
-                            "answer_start": 21,
-                        }
-                    ],
-                },
-                {
-                    "id": "00003",
-                    "is_impossible": True,
-                    "question": "What are Allomantic powers?",
-                    "answers": [],
-                },
-            ],
-        },
-    ]
+answers, probabilities = model.predict(to_predict)
+
+print(answers)
 
     # Configure the model
     model_args = QuestionAnsweringArgs()
