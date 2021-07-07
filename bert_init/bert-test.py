ff6cabe7c9a5cc6985d7b1497d6b17ab9dd0984d
David Li <david94306@gmail.com>
2021-07-07 06:37:17 -0700

    minor changes

--- bert_init/bert-test.py
+++ bert_init/bert-test.py
@@ -11,7 +11,8 @@ import csv
 import os
 import time
 from tqdm import tqdm
-
+import csv
+  
 
 
 class ParserModel(torch.nn.Module):
@@ -74,7 +75,10 @@ class ParserModel(torch.nn.Module):
 #              ['Fourth','Quarter','2020','Earnings','FEB','26','2021','AT','8:30','AM','EST','https://edge.media-server.com/mmc/p/6h793499']]
 
 event_tags = ['Shareholder Meeting','None','Earnings Release','Conference','Conference','Earnings Call','Stockholder Meeting','Conference','Conference','Shareholder Meeting',
-'Shareholder Meeting','Conference','Earnings Call','Earnings Call','Earnings Call','Earnings Release','Conference','Earnings Call','Earnings Release','Earnings Call']
+'Shareholder Meeting','Conference','Earnings Call','Earnings Call','Earnings Call','Earnings Release','Conference','Earnings Call','Earnings Release','Earnings Call','Earnings Release',
+'Earnings Release','Earnings Call','Earnings Release','Earnings Release','Shareholder Meeting','Earnings Release','Earnings Release','Earnings Call','Earnings Call','Earnings Call',
+'Earnings Release','Earnings Call','Earnings Call','None','Earnings Call','Earnings Release','Conference','Earnings Call','Earnings Release','Shareholder Meeting','Earnings Release','Conference',
+'Earnings Call','Earnings Call','Earnings Call','Merger/Acquisition','Earnings Call','Earnings Call','Earnings Call','Earnings Call','Earnings Release']
 
 
 raw_event_text = [["2021 Astronics Corporation Annual Meeting of Shareholders May 25, 2021 • 10:00am EDT Orlando, Florida"],
@@ -96,7 +100,40 @@ raw_event_text = [["2021 Astronics Corporation Annual Meeting of Shareholders Ma
 ["5/12/2021 Open Lending Bear and Bull Debates Conference Call"],
 ["May 6, 2021 at 5:00 PM EDT PDF Solutions, Inc. First Quarter Earnings Release Results Call View Webcast"],
 ["2021	3rd Quarter	 pdf 2021 Q3 Earnings Release	Listen to Q3FY2021 Webcast View the Presentation Slides View Transcript"],
-["Cnova’s 1st Quarter 2021 Financial Results Conference Call & Webcast 6 May 2021"]]
+["Cnova’s 1st Quarter 2021 Financial Results Conference Call & Webcast 6 May 2021"],
+["2021	4th Quarter	 pdf 2021 Q4 Earnings Release"],
+["Transcat 2021 3rd quarter earnings release"],
+["Collegium Pharmaceutical 2021 Q1 earnings call 05/06/2021 4:30 PM EDT"],
+["Itron 2021 Q1 earnings release 05/03/2021 10:00 AM EDT"],
+["eplus 2020 Q4 earnings release 02/03/2021 4:30 PM Eastern Time https://event.on24.com/wcc/r/2929350/375079A13DB8E9A35EA0D08E7B32AC02"],
+["Berkshire Hills Bancorp shareholder meeting 05/20/2021"],
+["Whitestone Reit 2020 Q4 earnings release 02/24/2021"],
+["BBVA USA Bancshares 2021 Q1 earnings release 04/30/2021 8:30 AM CDT"],
+["BrightView 2021 Q2 earnings call 05/06/2021 10:00 AM EST https://event.on24.com/wcc/r/3080115/0C88CA80C39716461742284AE9B1087F"],
+["ArcBest 2021 Q1 earnings call 05/04/2021 9:30 AM EDT 800 682-8539 https://event.webcasts.com/starthere.jsp?ei=1447325&tp_key=5e6751b165"],
+["Oasis Midstream 2021 Q1 earnings call may 06 2021 12:00 PM CDT https://www.webcaster4.com/Webcast/Page/1777/41033"],
+["Zynex 2020 Q4 + Y earnings release 02/25/2021 2:15 PM MST https://www.webcaster4.com/Webcast/Page/1487/40106"],
+["05.27.21 04:45 PM EDT	Q1 2022 Hess Earnings Conference Call"],
+["Bernstein Fireside Chat: Dorothea von Boxberg, CEO, Lufthansa Cargo Tuesday, May 18, 2021 10:00AM EDT | 3:00PM BST | 10:00PM HKT"],
+["Electronic Arts Q4 earnings call 05/11/2021 02:00 PM PT 866 324-3683 https://event.on24.com/wcc/r/3082295/44574BD8DABA0634EBAFFF035D3C8A3E"],
+["SB Financial Group 2020 Y earnings release 03/11/2021"],
+["Guess 2021 Q4 Earnings Calls 03/31/2021 04:45 PM EST https://edge.media-server.com/mmc/p/tg6sh5s5"],
+["7th Annual Communications Infrastructure Summit BOULDER, CO / VIRTUAL |AUG 09–12, 2021"],
+["Q1 2021 Aon plc Earnings Conference Call April 30, 2021 07:30 AM CST"],
+["3/11/2021 2020 Annual Report"],
+["Varian Investor Day 2019 Friday, November 15, 2019 Listen to the Webcast"],
+["May 5, 2021 Q121 Earnings Press Release"],
+["March 17, 2021 1:10 PM EDT	Oppenheimer 31st Annual Healthcare Conference"],
+["Misonix 2021 Q3 Earnings Calls 05/06/2021 4:30PM EST https://edge.media-server.com/mmc/p/c8ptybs2"],
+["Q1 2021 Lantheus Holdings, Inc. Earnings Conference Call Tuesday, May 4, 2021 at 8:00 AM EDT"],
+["Adaptive Biotechnologies First Quarter Financial Results May 5, 2021 at 4:30 PM EDT"],
+["efi + sirus capital group acquisition"],
+["Friday, April 30, 2021 9:00 AM ET Q1 Earnings Conference Call"],
+["May 4, 2021 4:30 PM EDT Pulmonx Q1 2021 Earnings Conference Call Click here for webcast"],
+["Q1 2021 Summit Hotel Properties Earnings Conference Call Wednesday, May 5, 2021 at 9:00 AM ET/8:00 AM CT Participant Toll-Free Dial-In Number: (877) 930-8101 Conference ID: 3758235"],
+["THURSDAY, APRIL 29, 2021 AT 8:30 AM ET Navios Maritime Partners First Quarter 2021 Earnings Conference Call Webcast Click Here for Webcast"],
+["May 5, 2021 at 10:30 AM EDT Avista Corporation Q1 2021 Earnings Conference Call Click here for webcast"],
+["May 06, 2021 Alleghany Corporation 2021 First Quarter Results"]]
 
 # 1. For mapping slots between ints and string
 intent2id = {intent:id for id,intent in enumerate(event_tags)}
@@ -159,7 +196,7 @@ optim = AdamW(model.parameters(), lr=5e-5)
 #                 attention_mask: torch.tensor,
 #                 token_type_ids: torch.tensor,
 #                 slot_labels: torch.tensor = None
-for epoch in range(3):
+for epoch in tqdm(range(3)):
     for batch in train_loader:
         optim.zero_grad()
         input_ids = batch['input_ids'].to(device)
@@ -174,6 +211,7 @@ for epoch in range(3):
         intent_loss.backward(retain_graph=True) #need to retain_graph  when working with multiple losses
         optim.step()
 
+#run predictions on new dataset currently running on training set
 model.eval()
 acc = 0
 predictions = []
@@ -199,6 +237,9 @@ def evaluate(model):
     #true = [id2tag[id.item()] for id in labels[0]]
     intent_prediction = [id2intent[id.item()] for id in intent_idxs]
     predictions.append(intent_prediction)
+    with open('predictions.csv', 'w+', newline ='') as f:
+        write = csv.writer(f)
+        write.writerows(predictions)
     print(intent_prediction)
     # print(slot_loss) 
 
