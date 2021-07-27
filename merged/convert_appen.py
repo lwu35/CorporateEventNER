d9c3bc8f6b8feddec6ff8527f2d7058697d8ea16
David Li <david94306@gmail.com>
2021-07-26 22:52:20 -0700

    convert fix

--- merged/convert_appen.py
+++ merged/convert_appen.py
@@ -6,33 +6,33 @@ df = pd.read_csv('appen_test.csv')
 
 please_select_the_event_type_gold = df['please_select_the_event_type_gold'].tolist()
 
-copy_the_company_name_from_the_text_gold = df['copy_the_company_name_from_the_text_gold'].astype(str).str.replace('\n', '').tolist()
+copy_the_company_name_from_the_text_gold = df['copy_the_company_name_from_the_text_gold'].astype(str).str.replace('\n', ' ').tolist()
 
 
-select_the_fiscal_period_check_as_many_as_it_applies_gold= df['select_the_fiscal_period_check_as_many_as_it_applies_gold'].astype(str).str.replace('\n', '').tolist()
+select_the_fiscal_period_check_as_many_as_it_applies_gold= df['select_the_fiscal_period_check_as_many_as_it_applies_gold'].astype(str).str.replace('\n', ' ').tolist()
 
 
 select_fiscal_year_gold = df['select_fiscal_year_gold'].tolist()
 
 enter_date_here_mmddyyyy_gold = df['enter_date_here_mmddyyyy_gold'].tolist()
 
-enter_time_here_hhmm_pmam_gold = df['enter_time_here_hhmm_pmam_gold'].astype(str).str.replace('\n', '').tolist()
+enter_time_here_hhmm_pmam_gold = df['enter_time_here_hhmm_pmam_gold'].astype(str).str.replace('\n', ' ').tolist()
 
 select_timezone_gold = df['select_timezone_gold'].tolist()
 
 please_select_the_event_type = df['_please_select_the_event_type'].tolist()
 
-copy_the_company_name_from_the_text = df['_copy_the_company_name_from_the_text'].astype(str).str.replace('\n', '').tolist()
+copy_the_company_name_from_the_text = df['_copy_the_company_name_from_the_text'].astype(str).str.replace('\n', ' ').tolist()
 
 
-select_the_fiscal_period_check_as_many_as_it_applies= df['_select_the_fiscal_period_check_as_many_as_it_applies'].astype(str).str.replace('\n', '').tolist()
+select_the_fiscal_period_check_as_many_as_it_applies= df['_select_the_fiscal_period_check_as_many_as_it_applies'].astype(str).str.replace('\n', ' ').tolist()
 
 
 select_fiscal_year = df['_select_fiscal_year'].tolist()
 
 enter_date_here_mmddyyyy = df['_enter_date_here_mmddyyyy'].tolist()
 
-enter_time_here_hhmm_pmam = df['_enter_time_here_hhmm_pmam'].astype(str).str.replace('\n', '').tolist()
+enter_time_here_hhmm_pmam = df['_enter_time_here_hhmm_pmam'].astype(str).str.replace('\n', ' ').tolist()
 
 select_timezone = df['_select_timezone'].tolist()
 
