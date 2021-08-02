608ccf7426be4db7e0a137a6af6262d14a3dd006
Liren Wu <lwu35@ucsc.edu>
2021-08-01 23:52:43 -0700

    add eval tuple

--- merged/evaluate.py
+++ merged/evaluate.py
@@ -3,9 +3,9 @@ import os
 import sys
 from tqdm.auto import tqdm
 from fuzzywuzzy import fuzz
-import configparser
+
 import datetime
-import logging
+
 from date_extractor import extract_dates
 from dateparser.search import search_dates
 
@@ -14,31 +14,17 @@ from sklearn.metrics import accuracy_score, classification_report, f1_score, pre
 import warnings
 warnings.filterwarnings("ignore")
 
-config = configparser.ConfigParser()
-config.read('configg.ini')
-
-log_file = open('eval_output.txt', 'w')
 
 def main():
 
     if len(sys.argv) == 3:
         print(f'Comparing {sys.argv[1]} and {sys.argv[2]}')
-        print(f'Comparing {sys.argv[1]} and {sys.argv[2]}',file = log_file)
         gold_file = sys.argv[1]
         pred_file = sys.argv[2]
     else:
         print('Comparing default files:')
-        print('Comparing default files:',file = log_file)
-        gold_file = config['pipeline']['INPUT_FILE']
-        et_model = config['pipeline']['event_type_model']
-        print('Using', et_model, 'for event type prediction')
-        print('Using', et_model, 'for event type prediction',file = log_file)
-        if et_model == 'bert':
-            pred_file = config['pipeline']['bert_predictions']
-        elif et_model == 'gpt':
-            pred_file = config['pipeline']['GPT_OUTPUT']
-        else:
-            pred_file = config['pipeline']['REGEX_OUTPUT']
+        gold_file = 'job2_gold.csv'
+        pred_file = 'bert_predictions.csv'
 
     file_path = os.path.join(gold_file)
     gold_df = pd.read_csv(file_path, sep=',', engine='python')
@@ -46,10 +32,116 @@ def main():
     file_path = os.path.join(pred_file)
     pred_df = pd.read_csv(file_path, sep=',', engine='python')
 
-    checker(gold_df, pred_df)
+    single_fields(gold_df, pred_df)
+    print('-------')
+    full_fields(gold_df, pred_df)
 
 
-def checker(gold_df, pred_df):
+def full_fields(gold_df, pred_df):
+
+    gold_event_id = list(gold_df['id'])
+    pred_event_id = list(pred_df['id'])
+
+    gold_company = list(gold_df['company'])
+    gold_fiscal_year = list(gold_df['fiscal_year'])
+    gold_fiscal_period = list(gold_df['fiscal_period'])
+    gold_event_type = list(gold_df['event_type'])
+    gold_date = list(gold_df['date'])
+    gold_time = list(gold_df['time'])
+    gold_timezone = list(gold_df['timezone'])
+
+    pred_company = list(pred_df['company'])
+    pred_fiscal_year = list(pred_df['fiscal_year'])
+    pred_fiscal_period = list(pred_df['fiscal_period'])
+    pred_event_type = list(pred_df['event_type'])
+    pred_date = list(pred_df['date'])
+    pred_time = list(pred_df['time'])
+    pred_timezone = list(pred_df['timezone'])
+
+    count_all = []
+    pred_all = []
+
+    # fix issues like this: 'Apr\xa029,\xa02021'
+    pred_date = date_space_checker(pred_date)
+
+    # Normalize fields
+    gold_fiscal_period, pred_fiscal_period = fiscal_period_converter(
+        gold_fiscal_period, pred_fiscal_period)
+    gold_date, pred_date = date_converter(gold_date, pred_date)
+    gold_time, pred_time = time_converter(gold_time, pred_time)
+    gold_timezone, pred_timezone = timezone_converter(
+        gold_timezone, pred_timezone)
+
+    for i in range(len(gold_event_id)):
+        if gold_event_id[i] != pred_event_id[i]:
+            print('EVENT ID MISMATCH')
+            break
+
+        cur_pred_all = []
+        cur_count_all = []
+        # company comparison
+        p_ratio = fuzz.partial_ratio(
+            pred_company[i].lower(), gold_company[i].lower())
+        ts_ratio = fuzz.token_set_ratio(pred_company[i], gold_company[i])
+
+        if p_ratio >= 90 or ts_ratio >= 90:
+            cur_pred_all.append(gold_company[i])
+        else:
+            cur_pred_all.append('WRONG')
+
+        cur_count_all.append(gold_company[i])
+
+        # fiscal year comparison
+        if pred_fiscal_year[i] == gold_fiscal_year[i]:
+            cur_pred_all.append(gold_fiscal_year[i])
+        else:
+            cur_pred_all.append(pred_fiscal_year[i])
+        cur_count_all.append(gold_fiscal_year[i])
+
+        # fiscal period comparison
+        if pred_fiscal_period[i] == gold_fiscal_period[i]:
+            cur_pred_all.append(gold_fiscal_period[i])
+        else:
+            cur_pred_all.append(pred_fiscal_period[i])
+        cur_count_all.append(gold_fiscal_period[i])
+
+        # event type comparison
+        if pred_event_type[i] == gold_event_type[i]:
+            cur_pred_all.append(gold_event_type[i])
+        else:
+            cur_pred_all.append(pred_event_type[i])
+        cur_count_all.append(gold_event_type[i])
+
+        # date comparison
+        if pred_date[i] == gold_date[i]:
+            cur_pred_all.append(gold_date[i])
+        else:
+            cur_pred_all.append(pred_date[i])
+        cur_count_all.append(gold_date[i])
+
+        # time comparison
+        if pred_time[i] == gold_time[i]:
+            cur_pred_all.append(gold_time[i])
+        else:
+            cur_pred_all.append(pred_time[i])
+        cur_count_all.append(gold_time[i])
+
+        # timezone comparison
+        if pred_timezone[i] == gold_timezone[i]:
+            cur_pred_all.append(gold_timezone[i])
+        else:
+            cur_pred_all.append(pred_timezone[i])
+        cur_count_all.append(gold_timezone[i])
+
+        count_all.append(str(tuple(cur_count_all)))
+        pred_all.append(str(tuple(cur_pred_all)))
+
+    print_score('Event tuples:', count_all, pred_all)
+
+    return 0
+
+
+def single_fields(gold_df, pred_df):
 
     gold_event_id = list(gold_df['id'])
     pred_event_id = list(pred_df['id'])
@@ -100,7 +192,6 @@ def checker(gold_df, pred_df):
     for i in range(len(gold_event_id)):
         if gold_event_id[i] != pred_event_id[i]:
             print('EVENT ID MISMATCH')
-            print('EVENT ID MISMATCH', file = log_file)
             break
 
         # company comparison
@@ -172,8 +263,8 @@ def checker(gold_df, pred_df):
     print_score('Timezone', gold_count_timezone, count_timezone)
 
     # print(confusion_matrix(gold_count_date, count_date))
-    print(classification_report(gold_count_fiscal_period, count_fiscal_period))
-    print(classification_report(gold_count_fiscal_period, count_fiscal_period),file = log_file)
+    # print(classification_report(gold_count_fiscal_period, count_fiscal_period))
+
     return 0
 
 
@@ -182,16 +273,10 @@ def print_score(field, gold_count, count):
         print(f'{field} \t\t({len(gold_count)} examples) \t\tAcc::', round(accuracy_score(
             gold_count, count), 3), f'\tF1:', round(f1_score(
                 gold_count, count, average='macro'), 3))
-        print(f'{field} \t\t({len(gold_count)} examples) \t\tAcc::', round(accuracy_score(
-            gold_count, count), 3), f'\tF1:', round(f1_score(
-                gold_count, count, average='macro'), 3),file = log_file)
     else:
         print(f'{field} \t({len(gold_count)} examples) \t\tAcc::', round(accuracy_score(
             gold_count, count), 3), f'\tF1:', round(f1_score(
                 gold_count, count, average='macro'), 3))
-        print(f'{field} \t({len(gold_count)} examples) \t\tAcc::', round(accuracy_score(
-            gold_count, count), 3), f'\tF1:', round(f1_score(
-                gold_count, count, average='macro'), 3),file = log_file)
 
 
 def date_converter(gold_dates, pred_dates):
