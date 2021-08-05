e5e99346654377224dccb95ba710d6161b2b5675
David Li <david94306@gmail.com>
2021-08-05 15:57:56 -0700

    new appen data

--- merged/convert_appen.py
+++ merged/convert_appen.py
@@ -1,7 +1,7 @@
 import csv
 import pandas as pd
 
-df = pd.read_csv('agg_116.csv')
+df = pd.read_csv('agg_85.csv')
 
 
 please_select_the_event_type_gold = df['please_select_the_event_type_gold'].tolist()
@@ -20,21 +20,21 @@ enter_time_here_hhmm_pmam_gold = df['enter_time_here_hhmm_pmam_gold'].astype(str
 
 select_timezone_gold = df['select_timezone_gold'].tolist()
 
-please_select_the_event_type = df['please_select_the_event_type'].tolist()
+please_select_the_event_type = df['please_select_the_event_type'].str.replace('_', ' ').tolist()
 
 copy_the_company_name_from_the_text = df['copy_the_company_name_from_the_text'].astype(str).str.replace('\n', ' ').tolist()
 
 
-select_the_fiscal_period_check_as_many_as_it_applies= df['select_the_fiscal_period_check_as_many_as_it_applies'].astype(str).str.replace('\n', ' ').tolist()
+select_the_fiscal_period_check_as_many_as_it_applies= df['select_the_fiscal_period_check_as_many_as_it_applies'].astype(str).str.replace('\n', ' ').str.replace('None/Other', 'NONE').str.replace('Quarter 1/Q1', 'Q1').str.replace('Quarter 2/Q2', 'Q2').str.replace('Quarter 3/Q3', 'Q3').str.replace('Quarter 4/Q4', 'Q4').tolist()
 
 
-select_fiscal_year = df['select_fiscal_year'].tolist()
+select_fiscal_year = df['select_fiscal_year'].str.replace('\n', ' ').str.replace('None', 'NONE').tolist()
 
 enter_date_here_mmddyyyy = df['enter_date_here_mmddyyyy'].tolist()
 
-enter_time_here_hhmm_pmam = df['enter_time_here_hhmm_pmam'].astype(str).str.replace('\n', ' ').tolist()
+enter_time_here_hhmm_pmam = df['enter_time_here_hhmm_pmam'].astype(str).str.replace('None', 'NONE').tolist()
 
-select_timezone = df['select_timezone'].tolist()
+select_timezone = df['select_timezone'].str.replace('None', 'NONE').str.replace('EDT/EST/ET', 'ET').str.replace('CDT/CST/CT', 'CDT').str.replace('MDT/MST/MT', 'MDT').str.replace('PDT/PST/PT', 'PDT').str.replace('Other', 'OTHER').tolist()
 
 id = df['id'].tolist()
 
@@ -51,7 +51,7 @@ for i in range(nums):
 
 
 #dict_gold = {'id': id, 'event_text': event_text, 'url': url, 'url_id': url_id, 'company': copy_the_company_name_from_the_text_gold, 'fiscal_year': select_fiscal_year_gold, 'fiscal_period': select_the_fiscal_period_check_as_many_as_it_applies_gold, 'event_type': please_select_the_event_type_gold, 'date': enter_date_here_mmddyyyy_gold, 'time': enter_time_here_hhmm_pmam_gold, 'timezone': select_timezone_gold}  
-dict = {'id': id, 'event_text': event_text, 'url': url, 'url_id': url_id, 'company': copy_the_company_name_from_the_text, 'fiscal_year': select_fiscal_year, 'fiscal_period': select_the_fiscal_period_check_as_many_as_it_applies, 'event_type': please_select_the_event_type, 'date': enter_date_here_mmddyyyy, 'time': enter_time_here_hhmm_pmam, 'timezone': select_timezone}  
+dict = {'id': ids, 'event_text': event_text, 'url': url, 'url_id': url_id, 'company': copy_the_company_name_from_the_text, 'fiscal_year': select_fiscal_year, 'fiscal_period': select_the_fiscal_period_check_as_many_as_it_applies, 'event_type': please_select_the_event_type, 'date': enter_date_here_mmddyyyy, 'time': enter_time_here_hhmm_pmam, 'timezone': select_timezone}  
 df = pd.DataFrame(dict)
 
-df.to_csv('cleaned_appen_116.csv')
+df.to_csv('cleaned_appen_85.csv')
