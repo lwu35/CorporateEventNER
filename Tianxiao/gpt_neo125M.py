d29d856215bf82da4633626209aa11cf866f311d
kassidin <706623929@qq.com>
2021-08-15 01:01:08 +0800

    Update gpt_neo125M.py

--- Tianxiao/gpt_neo125M.py
+++ Tianxiao/gpt_neo125M.py
@@ -7,14 +7,14 @@ from fuzzywuzzy import fuzz
 # Output: fine-tuned model
 def gpt_train(train_file):
     happy_gen = HappyGeneration("GPT-NEO", "EleutherAI/gpt-neo-125M")
-    args = GENTrainArgs(num_train_epochs=3)
+    args = GENTrainArgs(num_train_epochs=5)
     happy_gen.train(train_file, args=args)
     return happy_gen
 
 # Input: text content (string)
 # Output: event tag
 def gpt_predict(text,happy_gen):
-    tags_vocab = ['Other', 'Earnings Release', 'Earnings Call', 'Shareholder Meeting', 'Sales Results', 'Guidance', 'Conference', 'Merger/Acquisition']
+    tags_vocab = ['None/Other', 'Earnings Release', 'Earnings Call', 'Shareholder Meeting', 'Sales Results', 'Conference']
     test1 = "Text: " + text + "\n"
     question = 'Type:'
     top_k_sampling_settings = GENSettings(no_repeat_ngram_size=2,
@@ -22,8 +22,8 @@ def gpt_predict(text,happy_gen):
     result = happy_gen.generate_text(
         test1 + question, args=top_k_sampling_settings)
 
-    max_ratio = 0
-    max_tag = "Other"
+    max_ratio = 10
+    max_tag = "None/Other"
     for j in tags_vocab:
         ratio = fuzz.ratio(j, result.text.splitlines()[0])
 
@@ -37,7 +37,7 @@ def gpt_predict(text,happy_gen):
 # Input: a list of text content (string)
 # Output: a list ofevent tag
 def gpt_predicts(raw_event_text,happy_gen):
-    tags_vocab = ['Other', 'Earnings Release', 'Earnings Call', 'Shareholder Meeting', 'Sales Results', 'Guidance', 'Conference', 'Merger/Acquisition']
+    tags_vocab = ['None/Other', 'Earnings Release', 'Earnings Call', 'Shareholder Meeting', 'Sales Results', 'Conference']
     tags = []
     for i in range(len(raw_event_text)):
         test1 = "Text: " + raw_event_text[i][0] + "\n"
@@ -48,7 +48,7 @@ def gpt_predicts(raw_event_text,happy_gen):
             test1 + question, args=top_k_sampling_settings)
 
         max_ratio = 10
-        max_tag = "Other"
+        max_tag = "None/Other"
         for j in tags_vocab:
             ratio = fuzz.ratio(j, result.text.splitlines()[0])
 
@@ -59,12 +59,27 @@ def gpt_predicts(raw_event_text,happy_gen):
         # print(max_tag + '\n')
     return tags
 
-def main():
+if __name__ == "__main__":
     happy_gen = gpt_train('gpt_train.txt')
-    with open('gpt_test.txt', 'r', encoding='utf-8') as f:
+    happy_gen.save("model/")
+    with open('gpt_dev.txt', 'r', encoding='utf-8') as f:
         raw_event_text = []
         lines = f.readlines()
         for i in lines:
             raw_event_text.append([i.replace('\n', '')])
-    print(gpt_predicts(raw_event_text,happy_gen))
-
+    pred_types = gpt_predicts(raw_event_text,happy_gen)
+    with open('dev_eval.txt', 'r', encoding='utf-8') as f:
+        true_tpyes = []
+        lines = f.readlines()
+        for i in lines:
+            true_tpyes.append([i])
+    count = 0
+    for i in range(len(pred_types)):
+      if pred_types[i] == true_tpyes[i][0]:
+        count = count + 1
+      # else:
+      #   print(i)
+      #   print(pred_types[i])
+      #   print(true_tpyes[i][0])
+        
+    print("Accuracy:{:.2f}%".format(count/200*100))
